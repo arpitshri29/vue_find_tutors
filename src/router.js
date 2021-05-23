9ad47a9366a13fb8f073cb02fef9b639dea71912
arpitshri29 <shriarpit29@gmail.com>
2021-05-23 14:41:48 +0530

    - Components added - Routes updated

--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import {createRouter, createWebHistory} from "vue-router";
 
+import ContactTutor from './pages/requests/ContactTutor';
+import RequestReceived from './pages/requests/RequestReceived';
+import TutorDetail from './pages/tutors/TutorDetail';
+import TutorRegistration from './pages/tutors/TutorRegistration';
+import TutorsList from './pages/tutors/TutorsList';
+import NotFound from './pages/NotFound';
+
 // Create router by calling router
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/tutors'},
-        { path: '/tutors', component: null},
-        { path: '/tutors/:id', component: null, children: [
-            { path: '/contact', component: null},    // /tutors/t1/contact
+        { path: '/tutors', component: TutorsList},
+        { path: '/tutors/:id', component: TutorDetail, children: [
+            { path: '/contact', component: ContactTutor},    // /tutors/t1/contact
             ]
         },
-        { path: '/register', component: null},
-        { path: '/requests', component: null},
-        { path: '/:notFound(.*)', component: null},
+        { path: '/register', component: TutorRegistration},
+        { path: '/requests', component: RequestReceived},
+        { path: '/:notFound(.*)', component: NotFound},
     ],
 });
 
